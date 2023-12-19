0fae716fae00375385db5beca21688eb6866d82e
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-12-19 18:57:46 +0530

    Create ci.tf

---
+++ ci.tf
@@ -0,0 +1,49 @@
+resource "google_project_service" "enable_artifact_registry_api" {
+  service = "artifactregistry.googleapis.com"
+  project = "<YOUR_PROJECT_ID>"
+}
+
+
+resource "google_artifact_registry_repository" "cache" {
+  provider = google-beta
+  project  = module.google-cloud-project.project.project_id
+
+  location      = local.region
+  repository_id = "my-repo"
+  description   = "Repository for storing Docker images in the ${module.google-cloud-project.name}."
+
+  format = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest-release"
+    action = "KEEP"
+
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["latest"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
+
+  cleanup_policies {
+    id     = "gc-untagged"
+    action = "DELETE"
+
+    condition {
+      tag_state  = "UNTAGGED"
+      older_than = "${14 * 24 * 60 * 60}s"
+    }
+  }
+
+  depends_on = [
+    google_project_service.enable_artifact_registry_api
+  ]
+}
