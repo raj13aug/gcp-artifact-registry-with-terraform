a2b698bc72f36cc73a46d457441e684c817a3623
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-12-21 16:52:17 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,6 +1,55 @@
 locals {
-
   region            = "us-central1"
   availability_zone = "us-central1-a"
+}
 
+
+resource "google_project_service" "enable_artifact_registry_api" {
+  service = "artifactregistry.googleapis.com"
+  project = var.project_id
+}
+
+
+resource "google_artifact_registry_repository" "cache" {
+  provider = google-beta
+  project  = var.project_id
+
+  location      = local.region
+  repository_id = "my-repo"
+  description   = "Repository for storing Docker images."
+
+  format = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest-release"
+    action = "KEEP"
+
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["latest"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+
+    most_recent_versions {
+      keep_count = 5
+    }
+  }
+
+  cleanup_policies {
+    id     = "gc-untagged"
+    action = "DELETE"
+
+    condition {
+      tag_state  = "UNTAGGED"
+      older_than = "${14 * 24 * 60 * 60}s"
+    }
+  }
+
+  depends_on = [
+    google_project_service.enable_artifact_registry_api
+  ]
 }
