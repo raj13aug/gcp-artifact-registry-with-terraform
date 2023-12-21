71bd4975f7a852bcbbf250a2096cf408355bae53
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-12-21 17:03:59 +0530

    beta

--- provider.tf
+++ provider.tf
@@ -4,10 +4,23 @@ terraform {
       source  = "hashicorp/google"
       version = "5.10.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.10.0"
+    }
   }
 }
 
 provider "google" {
+  credentials = file("terraform_credentails.json")
+  project     = var.project_id
+  region      = "us-central1"
+  zone        = "us-central1-a"
+  #request_timeout = "60s"
+}
+
+
+provider "google-beta" {
   credentials = file("terraform_credentails.json")
   project     = var.project_id
   region      = "us-central1"
